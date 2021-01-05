707163a32b5c57b1f98fa57c2d25411798bae76e
anusu90 <anusu90@gmail.com>
2021-01-05 13:25:04 +0530

    first commit

---
+++ server.js
@@ -0,0 +1,10 @@
+const express = require('express');
+const app = express();
+const cors = require('cors');
+const bodyParser = require('body-parser');
+
+app.use(cors());
+app.use(bodyParser.json());
+app.listen(process.env.PORT || 3001 , function(){
+    console.log("Server started")
+})
